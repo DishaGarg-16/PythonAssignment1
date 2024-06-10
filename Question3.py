bc71d428512a689753d94307ac4c6b9e5afd4c33
Disha Garg <garg.16.disha@gmail.com>
2024-06-10 23:06:35 +0530

    Commit

---
+++ Question3.py
@@ -0,0 +1,4 @@
+#3. Write a python program that calculates the factorial of a given number
+import math
+num=int(input('Enter a number to find factorial: '))
+print("The factorial of number,",num,"is", math.factorial(num))
