c8447d6af32c8cf81dc35af05adc3ecbf7184ca9
Disha Garg <garg.16.disha@gmail.com>
2024-06-10 23:02:01 +0530

    COMMIT

---
+++ Question1.py
@@ -0,0 +1,4 @@
+#1. Write a program that takes two numbers as input and prints their sum.
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+print("The sum of 2 numbers is:", num1+num2)
---
+++ Question2.py
@@ -0,0 +1,7 @@
+#2. Write a python program that checks whether a given number is even or odd.
+odd=int(input("Enter a number: "))
+if odd%2!=0:
+    print("It's an odd number")
+else:
+    print("The number is not odd") 
+    
