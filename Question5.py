a0db374c1e06c57a03c20301473904a9646fc146
Disha Garg <garg.16.disha@gmail.com>
2024-06-10 23:17:21 +0530

    Commit

---
+++ Question5.py
@@ -0,0 +1,5 @@
+#5. Write a program that takes a string input from the user and writes it to a text file.
+Text=str(input("Enter a Text to the file: "))
+NewFile=open("D:/downloads next/TextFile.txt","w")
+print(Text, file=NewFile)
+
