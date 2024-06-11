da291c8405897f2e44b48ade921797d1a90d592a
Disha Garg <garg.16.disha@gmail.com>
2024-06-11 21:43:01 +0530

    Commit

---
+++ Question13.py
@@ -0,0 +1,4 @@
+#13. Write a program that asks the user for their birth year and calculates their age.
+birth_year=int(input("Enter your birth year: "))
+Age=2024-birth_year  # Subtracting birth year from the present year
+print("Your age is",Age)
