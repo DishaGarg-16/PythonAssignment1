7aad6a511bd86b168fe5a25a7cb094e969cbbdab
Disha Garg <garg.16.disha@gmail.com>
2024-06-13 20:51:36 +0530

    Commit

--- Question20.py
+++ Question20.py
@@ -1,7 +1,12 @@
 #20. Write a python program that takes a list of numbers and returns their sum.
-numbers=input("Enter the numbers ")
-
+numbers=input("Enter the numbers ")  # give space between the numbers for this solution
 list1=[]
+for i in numbers.split():
+    list1.append(int (i))
+print(list1)
+print(sum(list1))
+'''
+# Another solution
 list2=[]
 for i in numbers:
   list1.append(i)
@@ -9,10 +14,4 @@ for x in list1:
     list2.append(int(x))
 print(list2)
 print(sum(list2))
-
-'''
-for i in numbers.split():
-    list1.append(int (i))
-print(list1)
-print(sum(list1))
 '''
